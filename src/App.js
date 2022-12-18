0c8c6f92099ac829c91f9a7d9ab4dd8071c1ac53
adilhannouni <adilhannouni@gmail.com>
2022-12-17 23:13:48 -0700

    feat(Zoom): add couple components for event date and location

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
 import Featured from './components/featured';
+import EventInfo from './components/eventInfo'
 
 const App = () =>{
   return (
     <div className="App">
       <Header/>
-
       <div>
         <Featured/>
       </div>
-
+      <div>
+        <EventInfo/>
+      </div>
       <div style={{backgroundColor: 'red', height: '800px'}}></div>
       <div style={{backgroundColor: 'blue', height: '800px'}}></div>
       <div style={{backgroundColor: 'green', height: '800px'}}></div>
